a9ce539423a5b6fc1c6685dfd4e2106037544f5b
Akos Toth <kuttyer@gmail.com>
2022-09-18 22:12:42 +0200

    1.3

--- os_stat.py
+++ os_stat.py
@@ -6,4 +6,4 @@ path = './work_dir/'
 for file in os.listdir(path):
     data = os.stat(os.path.join(path, file))
     last_mod = datetime.datetime.fromtimestamp(data.st_mtime)
-    print(file, data.st_size, 'bytes ', last_mod, 'last mod date')
+    print(file, data.st_size, 'bytes ', last_mod.strftime("%Y-%m-%d %H:%M:%S"), 'last mod date')
