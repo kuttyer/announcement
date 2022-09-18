749a3cc7f9a3dcc135b7b4c98146f1b36478adf2
Akos Toth <kuttyer@gmail.com>
2022-09-18 21:25:33 +0200

    1.5

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ path = './work_dir/'
 
 
 def orig_name_finders():
+    renamed_file = 0
     with open(f'{path}/name_source.txt', 'r') as f:
         lines = (f.read().splitlines())
 
@@ -13,6 +14,9 @@ def orig_name_finders():
             if announcement == archived_name:
                 print(f'Finded a file on {announcement} name and renamed to {orig_name}.')
                 os.rename(os.path.join(path, announcement), os.path.join(path, orig_name))
+                renamed_file += 1
+
+    print(f'{renamed_file} files has been renamed!')
 
 
 orig_name_finders()
