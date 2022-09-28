eff53fbdd69a96cb89fcedfbfde1e77abcf67c68
Akos Toth <kuttyer@gmail.com>
2022-09-28 23:34:35 +0200

    1.7

--- os_stat.py
+++ os_stat.py
@@ -13,17 +13,17 @@ def amslist_to_tuple():
         amslist_to_tuple.splitted_line_tuple = tuple(splitted_line_list)
 
 
-def file_rename(backup_filename, size, last_mod_date):
-    hit = [item for item in amslist_to_tuple.splitted_line_tuple if item[1] == size and str(item[2])[0:16] == str(last_mod_date[0:16])]
-    print(hit[0][0])
-    os.rename(os.path.join(path, backup_filename), os.path.join(path, hit[0][0]))
-
-
 def file_rename_by_parameters():
     for file in os.listdir(path):
         data = os.stat(os.path.join(path, file))
         last_mod = datetime.datetime.fromtimestamp(data.st_mtime)
         file_rename(file, str(data.st_size), str(last_mod.strftime("%Y-%m-%d %H:%M")))
+
+
+def file_rename(backup_filename, size, last_mod_date):
+    hit = [item for item in amslist_to_tuple.splitted_line_tuple if item[1] == size and str(item[2])[0:16] == str(last_mod_date[0:16])]
+    print(hit[0][0])
+    os.rename(os.path.join(path, backup_filename), os.path.join(path, hit[0][0]))
 
 
 amslist_to_tuple()
