7610b96f229c44df5f4a55c0fafcb5c6c248b843
Akos Toth <kuttyer@gmail.com>
2023-01-24 21:57:28 +0100

    1.7

--- os_stat.py
+++ os_stat.py
@@ -11,20 +11,40 @@ def amslist_to_tuple():
         for line in x:
             splitted_line_list.append(line.strip().split(','))
         amslist_to_tuple.splitted_line_tuple = tuple(splitted_line_list)
+        print(amslist_to_tuple.splitted_line_tuple)
+
+
+def walk_through_process():
+    for item in amslist_to_tuple.splitted_line_tuple:
+        temp_name_list = []
+        #print(item[0])
+        for file in os.listdir(path):
+            file_stats = os.stat(os.path.join(path, file))
+            if str(file_stats.st_size) == str(item[1]):
+                temp_name_list.append(item[0])
+                if len(temp_name_list) == 1:
+                    print(temp_name_list)
 
 
 def file_rename_by_parameters():
+    i = 0
     for file in os.listdir(path):
-        data = os.stat(os.path.join(path, file))
-        last_mod = datetime.datetime.fromtimestamp(data.st_mtime)
-        file_rename(file, str(data.st_size), str(last_mod.strftime("%Y-%m-%d %H:%M")))
+        if file[0:2] == "c-":
+            data = os.stat(os.path.join(path, file))
+            last_mod = datetime.datetime.fromtimestamp(data.st_mtime)
+            i += 1
+            file_rename(file, data.st_size, last_mod.strftime("%Y-%m-%d %H:%M"), i)
 
 
-def file_rename(backup_filename, size, last_mod_date):
-    #print(backup_filename, size, last_mod_date)
+def file_rename(backup_filename, size, last_mod_date, i):
+    temp_name_list = []
+    #print(backup_filename, size, len(last_mod_date))
     for item in amslist_to_tuple.splitted_line_tuple:
-        print(str(item[2])[0:16], str(last_mod_date[0:16]))
-
+        if str(item[1]) == str(size):
+            temp_name_list.append(item[0])
+            if len(temp_name_list) == 1:
+                print(temp_name_list)
+        os.rename(os.path.join(path, backup_filename), os.path.join(path, item[0]+".wav"))
 
 
     #hit = [item for item in amslist_to_tuple.splitted_line_tuple if item[1] == size and str(item[2])[0:16] == str(last_mod_date[0:16])]
@@ -32,4 +52,5 @@ def file_rename(backup_filename, size, last_mod_date):
 
 
 amslist_to_tuple()
-file_rename_by_parameters()
+#file_rename_by_parameters()
+walk_through_process()
